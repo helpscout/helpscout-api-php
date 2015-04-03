a7e734066bd2ff031e616cb0a1eff990a7a14d30
Craig Davis <cadavis@gmail.com>
2015-04-03 07:27:50 -0600

    Add TravisCI and initial PHP Unit test harness

---
+++ tests/CreateConversationTest.php
@@ -0,0 +1,9 @@
+<?php
+class CreateConversationTest extends TestCase {
+    public function testBasicExample()
+    {
+        $this->assertEquals(200, 200);
+    }
+}
+
+/* End of file CreateConversationTest.php */
---
+++ tests/TestCase.php
@@ -0,0 +1,6 @@
+<?php
+class TestCase extends \PHPUnit_Framework_TestCase
+{
+}
+
+/* End of file TestCase.php */
