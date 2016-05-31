9d5265581c8903c9dfafce7cff8a2185fcf83785
Chris Duell <me@chrisduell.com>
2016-05-31 17:09:00 +1000

    Removed `final` from class declaration
    To allow for people to extend the class, in the event they want to for example use it as the base class for creating a Docs API

--- src/HelpScout/ApiClient.php
+++ src/HelpScout/ApiClient.php
@@ -5,7 +5,7 @@ require_once 'ClassLoader.php';
 
 use HelpScout\model\Attachment;
 
-final class ApiClient {
+class ApiClient {
 	const USER_AGENT = 'Help Scout API/Php Client v1';
 	const API_URL    = 'https://api.helpscout.net/v1/';
 	const NAMESPACE_SEPARATOR = '\\';
@@ -46,7 +46,7 @@ final class ApiClient {
 	 */
 	public static function getInstance() {
 		if (self::$instance === false) {
-			self::$instance = new ApiClient();
+			self::$instance = new static();
 		}
 		return self::$instance;
 	}
