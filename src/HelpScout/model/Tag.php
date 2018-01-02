e8f3d0b67ef4619a5a2d658c8a812dd26b58bf10
Steven Wade <stevenwadejr@gmail.com>
2018-01-02 12:24:50 -0500

    Format Tag.php code

--- src/HelpScout/model/Tag.php
+++ src/HelpScout/model/Tag.php
@@ -2,62 +2,71 @@
 
 namespace Helpscout\model;
 
-class Tag {
+class Tag
+{
     private $id;
     private $tag;
     private $count;
     private $color;
     private $slug;
-	private $createdAt;
-	private $modifiedAt;
-	public function __construct($data = null) {
-		$properties = array('id', 'tag', 'count', 'color', 'slug', 'createdAt', 'modifiedAt');
-		if ($data) {
-			foreach ($properties as $property) {
-				$this->$property = isset($data->$property) ? $data->$property : null;
-			}
-		}
-	}
+    private $createdAt;
+    private $modifiedAt;
 
-	/**
-	 * @return int
-	 */
-	public function getId() {
-		return $this->id;
-	}
+    public function __construct($data = null)
+    {
+        $properties = array('id', 'tag', 'count', 'color', 'slug', 'createdAt', 'modifiedAt');
+        if ($data) {
+            foreach ($properties as $property) {
+                $this->$property = isset($data->$property) ? $data->$property : null;
+            }
+        }
+    }
 
-	/**
-	 * @return the $tag
-	 */
-	public function getTag() {
-		return $this->tag;
-	}
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
 
-	/**
-	 * @return the $slug
-	 */
-	public function getSlug() {
-		return $this->slug;
-	}
+    /**
+     * @return the $tag
+     */
+    public function getTag()
+    {
+        return $this->tag;
+    }
 
-	/**
-	 * @return the $color
-	 */
-	public function getColor() {
-		return $this->color;
-	}
+    /**
+     * @return the $slug
+     */
+    public function getSlug()
+    {
+        return $this->slug;
+    }
 
-	/**
-	 * @return the $modifiedAt
-	 */
-	public function getModifiedAt() {
-		return $this->modifiedAt;
-	}
+    /**
+     * @return the $color
+     */
+    public function getColor()
+    {
+        return $this->color;
+    }
 
-	/**
-	 * @return the $createdAt
-	 */
-	public function getCreatedAt() {
-		return $this->createdAt;
-	}
+    /**
+     * @return the $modifiedAt
+     */
+    public function getModifiedAt()
+    {
+        return $this->modifiedAt;
+    }
+
+    /**
+     * @return the $createdAt
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
 }
