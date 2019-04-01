589163d017b2bbf81e60a3d024125d598f22bf41
Ben Kuhl <benkuhl@helpscout.com>
2019-04-01 12:50:38 -0400

    Added example for how to create a Customer

--- examples/customers.php
+++ examples/customers.php
@@ -4,9 +4,21 @@ require '_credentials.php';
 
 use HelpScout\Api\ApiClientFactory;
 use HelpScout\Api\Customers\CustomerFilters;
+use HelpScout\Api\Customers\Customer;
+use HelpScout\Api\Customers\Entry\Email;
 
 $client = ApiClientFactory::createClient();
 $client = $client->useClientCredentials($appId, $appSecret);
+
+// Create Customer
+$customer = new Customer();
+$customer->setFirstName('John');
+$customer->setLastName('Smith');
+$customer->addEmail(new Email([
+    'email' => "my-customer@their-business.com",
+    'type' => 'work',
+]));
+$client->customers()->create($customer);
 
 // GET customers
 $customer = $client->customers()->get(161694345);
