21f25d74f37eb891117a38d45daa0cb9f017841b
David Stanley <davidstanley01@gmail.com>
2019-04-09 06:46:47 -0500

    Validate webhooks for parity with v1 SDK (#122)
    * Add methods to process incoming webhooks
    
    * Fix style issues
    
    * Add tests
    
    * Add continue statement and use other header type
    
    * Update readme
    
    * Add example

---
+++ examples/incoming_webhook.php
@@ -0,0 +1,24 @@
+<?php
+
+use GuzzleHttp\Psr7\Request;
+use HelpScout\Api\Webhooks\IncomingWebhook;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$body = json_encode(['id' => 123]);
+$secret = 'asdfasdf';
+$signature = '7S2+kN8bZsrdAEADJ+yYfWWsf/4=';
+
+$headers = [
+    'HTTP_X_HELPSCOUT_SIGNATURE' => $signature,
+    'HTTP_X_HELPSCOUT_EVENT' => 'convo.deleted',
+];
+
+$request = new Request('POST', 'www.blah.blah', $headers, $body);
+$webhook = new IncomingWebhook($request, $secret);
+
+$eventType = $webhook->getEventType();
+if ($eventType === 'convo.deleted') {
+    $obj = $webhook->getDataObject();
+    echo $obj->id;
+}
