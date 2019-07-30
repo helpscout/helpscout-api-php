82377cdd3d2d9025f948d89ef7e0170202ac6e1c
Ben Kuhl <benkuhl@helpscout.com>
2019-07-30 12:36:36 -0400

    v2.3.8

--- src/ApiClient.php
+++ src/ApiClient.php
@@ -21,7 +21,7 @@ use HelpScout\Api\Workflows\WorkflowsEndpoint;
 
 class ApiClient
 {
-    public const CLIENT_VERSION = '2.3.7';
+    public const CLIENT_VERSION = '2.3.8';
 
     public const AVAILABLE_ENDPOINTS = [
         'hs.workflows' => WorkflowsEndpoint::class,
