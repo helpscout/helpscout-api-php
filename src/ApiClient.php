213b21daf79dab84869dfab3f0bab1a6fec6d697
Ben Kuhl <benkuhl@helpscout.com>
2019-08-08 14:44:46 -0400

    v2.3.9

--- src/ApiClient.php
+++ src/ApiClient.php
@@ -22,7 +22,7 @@ use Mockery\LegacyMockInterface;
 
 class ApiClient
 {
-    public const CLIENT_VERSION = '2.3.8';
+    public const CLIENT_VERSION = '2.3.9';
 
     public const AVAILABLE_ENDPOINTS = [
         'hs.workflows' => WorkflowsEndpoint::class,
