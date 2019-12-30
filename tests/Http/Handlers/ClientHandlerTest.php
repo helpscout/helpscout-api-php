e02fad4c2f2f6394934cc3ed9220bd6ade88803c
Anthony Terrell <ant@MacBookPro.local>
2019-12-30 10:31:48 -0600

    Add test coverage for ClientHandler

--- tests/Http/Handlers/ClientHandlerTest.php
+++ tests/Http/Handlers/ClientHandlerTest.php
@@ -4,7 +4,14 @@ declare(strict_types=1);
 
 namespace HelpScout\Api\Tests\Http\Handlers;
 
+use GuzzleHttp\Client;
 use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use HelpScout\Api\ApiClient;
+use HelpScout\Api\Customers\Customer;
+use HelpScout\Api\Http\Authenticator;
+use HelpScout\Api\Http\RestClient;
 use HelpScout\Api\Tests\ApiClientIntegrationTestCase;
 
 class ClientHandlerTest extends ApiClientIntegrationTestCase
@@ -34,5 +41,26 @@ class ClientHandlerTest extends ApiClientIntegrationTestCase
         $this->stubResponse($this->getResponse(404, json_encode([])));
 
         $this->client->customers()->get(1);
+    }
+
+    public function testRequestExceptionNotThrownWhenNotAuthorized()
+    {
+        $this->mockHandler = new MockHandler();
+        $handler           = HandlerStack::create($this->mockHandler);
+
+        $client              = new Client(['handler' => $handler]);
+        $this->authenticator = new Authenticator($client);
+        $this->authenticator->setAccessToken('abc123');
+
+        $this->client = new ApiClient(
+            new RestClient($client, $this->authenticator)
+        );
+
+        $this->stubResponse($this->getResponse(401, json_encode([])));
+
+        $this->assertInstanceOf(
+            Customer::class,
+            $this->client->customers()->get(1)
+        );
     }
 }
