6c0a16f9a4e91f3a23d7adc13dbfd61cbc70d4b9
Ben <benkuhl@helpscout.com>
2020-12-08 12:16:22 -0500

    Ordering traits alphabetically

--- src/Workflows/Workflow.php
+++ src/Workflows/Workflow.php
@@ -12,8 +12,8 @@ use HelpScout\Api\Support\HydratesData;
 
 class Workflow implements Hydratable, Extractable
 {
-    use HydratesData,
-        ExtractsData;
+    use ExtractsData,
+        HydratesData;
 
     public const TYPE_MANUAL = 'manual';
     public const TYPE_AUTOMATIC = 'automatic';
